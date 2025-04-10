90eecfb3c079237ebb5ca46e6dae73eda7ce3abc
Mihály Neumann <mihaly.neumann@glosterdigital.com>
2025-04-10 09:34:41 +0200

    implement composite array parsing, rework struct parser and fix issues, add tests

--- functions/structureparsing.go
+++ functions/structureparsing.go
@@ -7,13 +7,13 @@ import (
 	"reflect"
 )
 
-func ParseStruct(inputLines []string, targetStruct interface{}, lineIndex int, depth int, config *models.Configuration) (err error) {
+func ParseStruct(inputLines []string, targetStruct interface{}, lineIndex *int, depth int, config *models.Configuration) (err error) {
 	// Check for maximum depth
 	if depth >= constants.MAX_DEPTH {
 		return errmsg.StructureParsing_ErrMaxDepthReached
 	}
 	// Check for enough input lines
-	if lineIndex >= len(inputLines) {
+	if *lineIndex >= len(inputLines) {
 		return errmsg.StructureParsing_ErrInputLinesDepleted
 	}
 
@@ -29,68 +29,57 @@ func ParseStruct(inputLines []string, targetStruct interface{}, lineIndex int, d
 		// Save the target value pointer
 		targetValue := targetValues[i].Addr().Interface()
 
-		// If the target is a composite structure: no lines to parse yet, just go further down the rabbit hole
-		//TODO: handle optional fields
-		if targetStructAnnotation.IsComposite {
-			if targetStructAnnotation.IsArray {
-				// Iterate as long as we have matching input structure
-				for {
-					// Recursively parse the composite structure
-					err = ParseStruct(inputLines, targetValue, lineIndex, depth+1, config)
+		//TODO: handle optional elements
+
+		// Target is an array it is iterated with conditional break (unknown length)
+		if targetStructAnnotation.IsArray {
+			// Create the array structure
+			sliceType := reflect.SliceOf(targetValues[i].Type().Elem())
+			targetValues[i].Set(reflect.MakeSlice(sliceType, 0, 0))
+
+			// Iterate as long as we have matching input structure and still have input lines
+			for j := 1; *lineIndex < len(inputLines); j++ {
+				// Create a new element for the slice to parse into
+				elem := reflect.New(targetValues[i].Type().Elem()).Elem()
+
+				if targetStructAnnotation.IsComposite {
+					// Composite target: recursively parse the composite structure
+					err = ParseStruct(inputLines, elem.Addr().Interface(), lineIndex, depth+1, config)
+				} else {
+					// Non-composite target: parse the line into the new element
+					err = ParseLine(inputLines[*lineIndex], elem.Addr().Interface(), targetStructAnnotation.StructName, j, config)
+					// Increment the line index
+					*lineIndex++
+				}
+
+				if err != nil {
 					// TODO: handle structure change better than a specific error
-					// TODO: handle end of input lines error and non-error (end of array) cases
-					if err != nil {
+					if err == errmsg.LineParsing_ErrLineTypeNameMismatch {
 						// If the error is a line type name mismatch, it means the end of the array
-						if err == errmsg.LineParsing_ErrLineTypeNameMismatch {
-							break
-						} else {
-							return err
-						}
+						break
+					} else {
+						// Other error
+						return err
 					}
 				}
-			} else {
-				// Recursively parse the composite structure
+				// If no error, add the new element to the slice
+				targetValues[i].Set(reflect.Append(targetValues[i], elem))
+			}
+		} else {
+			// Single element structure
+			if targetStructAnnotation.IsComposite {
+				// Composite target: go further down the rabbit hole
 				err = ParseStruct(inputLines, targetValue, lineIndex, depth+1, config)
 				if err != nil {
 					return err
 				}
-			}
-			// Target is not composite, there is a line to parse
-		} else {
-			if targetStructAnnotation.IsArray {
-				// Create the array structure if the target is an array
-				sliceType := reflect.SliceOf(targetValues[i].Type().Elem())
-				targetValues[i].Set(reflect.MakeSlice(sliceType, 0, 0))
-
-				// Iterate as long as we have matching input structure and still have input lines
-				for j := 1; lineIndex < len(inputLines); j++ {
-					// Create a new element for the slice to parse into
-					elem := reflect.New(targetValues[i].Type().Elem()).Elem()
-
-					// Parse the line into the new element
-					err = ParseLine(inputLines[lineIndex], elem.Addr().Interface(), targetStructAnnotation.StructName, j, config)
-
-					if err != nil {
-						// TODO: handle structure change better than a specific error
-						if err == errmsg.LineParsing_ErrLineTypeNameMismatch {
-							// If the error is a line type name mismatch, it means the end of the array
-							break
-						} else {
-							// Other error
-							return err
-						}
-					}
-					// If no error, add the new element to the slice
-					targetValues[i].Set(reflect.Append(targetValues[i], elem))
-
-					// Increment the line index too
-					lineIndex++
-				}
-				// Plain old single line structure to parse
 			} else {
-				// Parse the line and increment the line index
-				err = ParseLine(inputLines[lineIndex], targetValue, targetStructAnnotation.StructName, 1, config)
-				lineIndex++
+				// Non-composite target: there is a single line to parse
+				if *lineIndex >= len(inputLines) {
+					return errmsg.StructureParsing_ErrInputLinesDepleted
+				}
+				err = ParseLine(inputLines[*lineIndex], targetValue, targetStructAnnotation.StructName, 1, config)
+				*lineIndex++
 				if err != nil {
 					return err
 				}
--- functions/structureparsing_test.go
+++ functions/structureparsing_test.go
@@ -1,6 +1,7 @@
 package functions
 
 import (
+	"github.com/blutspende/go-astm/v2/errmsg"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -20,8 +21,9 @@ func TestParseStruct_SingleLineStruct(t *testing.T) {
 		"R|1|first|second",
 	}
 	target := SingleRecordStruct{}
+	lineIndex := 0
 	// Act
-	err := ParseStruct(input, &target, 0, 0, config)
+	err := ParseStruct(input, &target, &lineIndex, 0, config)
 	// Assert
 	assert.Nil(t, err)
 	assert.Equal(t, "first", target.FirstRecord.First)
@@ -39,8 +41,9 @@ func TestParseStruct_RecordArrayStruct(t *testing.T) {
 		"R|2|first2|second2",
 	}
 	target := RecordArrayStruct{}
+	lineIndex := 0
 	// Act
-	err := ParseStruct(input, &target, 0, 0, config)
+	err := ParseStruct(input, &target, &lineIndex, 0, config)
 	// Assert
 	assert.Nil(t, err)
 	assert.Len(t, target.RecordArray, 2)
@@ -73,8 +76,9 @@ func TestParseStruct_CompositeMessage(t *testing.T) {
 		"S|1|r2 first|r2 second",
 	}
 	target := CompositeMessage{}
+	lineIndex := 0
 	// Act
-	err := ParseStruct(input, &target, 0, 0, config)
+	err := ParseStruct(input, &target, &lineIndex, 0, config)
 	// Assert
 	assert.Nil(t, err)
 	assert.Equal(t, "r1 first", target.CompositeRecordStruct.Record1.First)
@@ -83,7 +87,61 @@ func TestParseStruct_CompositeMessage(t *testing.T) {
 	assert.Equal(t, "r2 second", target.CompositeRecordStruct.Record2.Second)
 }
 
-//TODO: composite array test
+type CompositeArrayMessage struct {
+	CompositeRecordArray []CompositeRecordStruct
+}
+
+func TestParseStruct_CompositeArrayMessage(t *testing.T) {
+	// Arrange
+	input := []string{
+		"F|1|a1 r1 first|a1 r1 second",
+		"S|1|a1 r2 first|a1 r2 second",
+		"F|1|a2 r1 first|a2 r1 second",
+		"S|1|a2 r2 first|a2 r2 second",
+	}
+	target := CompositeArrayMessage{}
+	lineIndex := 0
+	// Act
+	err := ParseStruct(input, &target, &lineIndex, 0, config)
+	// Assert
+	assert.Nil(t, err)
+	assert.Len(t, target.CompositeRecordArray, 2)
+	assert.Equal(t, "a1 r1 first", target.CompositeRecordArray[0].Record1.First)
+	assert.Equal(t, "a1 r1 second", target.CompositeRecordArray[0].Record1.Second)
+	assert.Equal(t, "a1 r2 first", target.CompositeRecordArray[0].Record2.First)
+	assert.Equal(t, "a1 r2 second", target.CompositeRecordArray[0].Record2.Second)
+	assert.Equal(t, "a2 r1 first", target.CompositeRecordArray[1].Record1.First)
+	assert.Equal(t, "a2 r1 second", target.CompositeRecordArray[1].Record1.Second)
+	assert.Equal(t, "a2 r2 first", target.CompositeRecordArray[1].Record2.First)
+	assert.Equal(t, "a2 r2 second", target.CompositeRecordArray[1].Record2.Second)
+}
+
+func TestParseStruct_UnexpectedLineTypeError(t *testing.T) {
+	// Arrange
+	input := []string{
+		"F|1|r1 first|r1 second",
+		"U|1|r2 first|r2 second",
+	}
+	target := CompositeMessage{}
+	lineIndex := 0
+	// Act
+	err := ParseStruct(input, &target, &lineIndex, 0, config)
+	// Assert
+	assert.Error(t, err, errmsg.LineParsing_ErrLineTypeNameMismatch)
+}
+
+func TestParseStruct_LinesDepletedError(t *testing.T) {
+	// Arrange
+	input := []string{
+		"F|1|r1 first|r1 second",
+	}
+	target := CompositeMessage{}
+	lineIndex := 0
+	// Act
+	err := ParseStruct(input, &target, &lineIndex, 0, config)
+	// Assert
+	assert.Error(t, err, errmsg.StructureParsing_ErrInputLinesDepleted)
+}
 
 // Note: config is setup in line parser test
 // TODO: separate test packages better
